c0074bd4aeec3751cd4a24b639558e8d21f83349
sw-yx <shawnthe1@gmail.com>
2022-12-11 18:34:18 -0500

    commit initial og

---
+++ mdsvex.config.js
@@ -0,0 +1,17 @@
+import { defineMDSveXConfig as defineConfig } from 'mdsvex';
+
+const config = defineConfig({
+	extensions: ['.svelte.md', '.md', '.svx'],
+	highlight: {
+		alias: { js: "javascript" }
+	},
+
+	smartypants: {
+		dashes: 'oldschool'
+	},
+
+	remarkPlugins: [],
+	rehypePlugins: []
+});
+
+export default config;
---
+++ src/routes/testpage/+page.md
@@ -0,0 +1,18 @@
+# hi
+
+alkdjslkjsd
+
+
+```js
+// Module dependencies.
+var mongoose = require('mongoose')
+  , Schema = mongoose.Schema
+  , ObjectId = mongoose.SchemaTypes.ObjectId
+  , SchemaType = mongoose.SchemaType
+
+var ObjectIdSchema = new Schema({
+  
+}
+```
+
+asdlkjasd
